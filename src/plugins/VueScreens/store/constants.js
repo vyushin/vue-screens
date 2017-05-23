850772c5027d7728dfda86c2603f0ac38bd057c5
vyushin <e@vyushin.ru>
2017-05-23 18:01:32 +0300

    r-1.0.4 Added tests

--- src/plugins/VueScreens/store/constants.js
+++ src/plugins/VueScreens/store/constants.js
@@ -1,11 +1,13 @@
-const   VS_OPTIONS      = 'VS_Options',
-        VS_SCREENS      = 'VS_Screens',
-        VS_RM_SCREENS   = 'VS_RmScreens',
-        VS_ADD_SCREEN   = 'VS_AddScreen',
-        VS_SHUFFLE      = 'VS_Shuffle';
+const   VS_OPTIONS      = 'VueScreens/Options',
+        VS_SCREENS      = 'VueScreens/Screens',
+        VS_RM_SCREENS   = 'VueScreens/RmScreens',
+        VS_ADD_SCREEN   = 'VueScreens/AddScreen',
+        VS_SHUFFLE      = 'VueScreens/Shuffle';
 
-export {VS_OPTIONS};
-export {VS_SCREENS};
-export {VS_RM_SCREENS};
-export {VS_ADD_SCREEN};
-export {VS_SHUFFLE};
+export {
+    VS_OPTIONS,
+    VS_SCREENS,
+    VS_RM_SCREENS,
+    VS_ADD_SCREEN,
+    VS_SHUFFLE
+};
--- src/plugins/VueScreens/store/index.js
+++ src/plugins/VueScreens/store/index.js
@@ -1,11 +1,11 @@
 import util                 from '../util';
-import {VS_OPTIONS,
-         VS_SCREENS,
-          VS_RM_SCREENS,
-           VS_ADD_SCREEN,
-            VS_SHUFFLE}     from './constants';
+import * as CONSTANTS       from './constants';
+
+const _CONSTANTS = {};
+Object.keys(CONSTANTS).map(key => _CONSTANTS[key] = CONSTANTS[key].replace(/^VueScreens\//, ''));
 
 export default {
+    namespaced: true,
     state: {
         options: {},
         screens: []
@@ -16,7 +16,7 @@ export default {
          * @param {Object} state State of VUEX
          * @param {Array} options Some new options for vue-screens plugin
          */
-        [VS_OPTIONS](state, options) {
+        [_CONSTANTS.VS_OPTIONS](state, options) {
             state.options = Object.assign(state.options, options);
         },
         /**
@@ -24,7 +24,7 @@ export default {
          * @param {Object} state State of VUEX
          * @param {Array|Object} screens
          */
-        [VS_ADD_SCREEN](state, screens) {
+        [_CONSTANTS.VS_ADD_SCREEN](state, screens) {
             if (util.isObject(screens)) {
                 state.screens.push(screens);
             } else if (util.isArray(screens)) {
@@ -32,10 +32,10 @@ export default {
                 state.screens = screens;
             }
         },
-        [VS_RM_SCREENS](state) {
+        [_CONSTANTS.VS_RM_SCREENS](state) {
 
         },
-        [VS_SHUFFLE](state) {
+        [_CONSTANTS.VS_SHUFFLE](state) {
             state.screens = util.shuffle(state.screens);
         }
     },
@@ -44,12 +44,12 @@ export default {
          * Returns public options of vue-screens plugin from state
          * @return {Object}
          */
-        [VS_OPTIONS]: (state) => (state.options),
+        [_CONSTANTS.VS_OPTIONS]: (state) => (state.options),
         /**
          * Returns screens of vue-screens plugin from state
          * @return {Array} Array of objects
          */
-        [VS_SCREENS]: (state) => (state.screens)
+        [_CONSTANTS.VS_SCREENS]: (state) => (state.screens)
     },
     actions: {}
 }
